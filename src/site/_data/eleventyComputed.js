8667d5afc532a31d9ca36da9bd2ecf0969a3b76e
HZanchi <heitorzanchi0610@gmail.com>
2026-03-17 15:20:29 -0300

    Update eleventyComputed.js

--- src/site/_data/eleventyComputed.js
+++ src/site/_data/eleventyComputed.js
@@ -1,5 +1,9 @@
 module.exports = {
   permalink: data => {
+    // Se já tiver permalink definido (como o seu), respeita
+    if (data.permalink) return data.permalink;
+
+    // Senão, gera automático baseado no nome do arquivo
     return `${data.page.fileSlug}/index.html`;
   }
 };

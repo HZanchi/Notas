43cf59c18348082d7091622e3eb2dafee68a6c6b
HZanchi <heitorzanchi0610@gmail.com>
2026-03-17 14:59:39 -0300

    Update eleventyComputed.js

--- src/site/_data/eleventyComputed.js
+++ src/site/_data/eleventyComputed.js
@@ -1,13 +1,3 @@
-const { getGraph } = require("../../helpers/linkUtils");
-const { getFileTree } = require("../../helpers/filetreeUtils");
-const { userComputed } = require("../../helpers/userUtils");
-
-module.exports = {
-  graph: async (data) => await getGraph(data),
-  filetree: (data) => getFileTree(data),
-  userComputed: (data) => userComputed(data)
-};
-
 module.exports = {
   permalink: data => {
     return `${data.page.fileSlug}/index.html`;
